76d293c9035aa36c53968a0a8de97b4c860b7261
zachnewburgh <zach.newburgh@gmail.com>
2016-10-19 10:06:02 -0400

    Configuring the assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,10 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.assets.compile = true
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
